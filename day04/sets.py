b78c54c246b17c3f5ca6c48a8e38b913ba584280
Caglar Engin <smyrna.caglar@gmail.com>
2023-09-21 17:37:09 +0200

    day4 sets finished

---
+++ day04/sets.py
@@ -0,0 +1,37 @@
+unique_element=set()
+
+
+print(type(unique_element))
+
+unique_element.add(10)
+unique_element.add(10)
+unique_element.add(10)
+unique_element.add(20)
+unique_element.add(20)
+unique_element.add(10)
+
+print(unique_element)  ##{10,20}
+
+#print(unique_element[1])
+
+#print(unique_element[1:])
+
+unique_element.remove(10)
+
+print(unique_element)
+
+unique_element.update((1,2,3,3,1,4,5))
+
+print(unique_element)
+
+unique_element.pop()
+
+print(unique_element)
+
+n = unique_element.pop()
+
+print(unique_element)
+print(n)
+
+print('----------------------')
+
